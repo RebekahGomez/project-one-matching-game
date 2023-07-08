9e5b2b184a09220f33243d6ca1cd0189fcd4070b
Rebekah <rebekah@Rebekahs-MacBook-Pro.local>
2023-07-08 15:43:31 -0400

    set up basic board in html & css

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create a deck of cards with identifiable images
+// Have the game automatically set up with the cards already shuffled, facing down
+// Create functions that allow cards to be flipped over and taken off 
+//   the board if they match OR flipped back automatically if they don't match
+// Computer should keep track of the score
+// Display whose turn it currently is
+// Computer will announce the winner or if there is a tie
+// Have a "reset" button that automatically shuffles the cards and disperses them facedown.
