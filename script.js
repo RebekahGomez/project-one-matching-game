890457387c2ff23d714e1f5c82086793331b6503
Rebekah <rebekah@Rebekahs-MacBook-Pro.local>
2023-07-08 16:59:22 -0400

    added pseudocode

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// Create a deck of cards with identifiable images
-// Have the game automatically set up with the cards already shuffled, facing down
-// Create functions that allow cards to be flipped over and taken off 
-//   the board if they match OR flipped back automatically if they don't match
-// Computer should keep track of the score
+// Function gameStart - The game should start with the cards already
+//    shuffled & face down & scores set to 0
+// Create a deck of cards
+// Each card should have an identifiable image - the image will be in the deck twice
+// Function to Restart + Shuffle
+// Each player gets to select 2 cards each turn
+// Function for error handling
+//   - players can't flip a third card & MUST flip 2 cards
+//   - players can't select the same card twice in one turn - if the card is flipped up
+//     it can't be selected again
+// Function to flip the cards
+// If the cards match, they get removed from the board
+// If the cards don't match, they flip back over - maybe use setTimeout
+//    so the users can actually see the cards before they are removed or flip back over
+// Function to remove the cards from the board
+// Function to switch players each turn
+// Function to track the players' scores
 // Display whose turn it currently is
+// Function to determine gameEnd - check if all pairs have been found
 // Computer will announce the winner or if there is a tie
-// Have a "reset" button that automatically shuffles the cards and disperses them facedown.
